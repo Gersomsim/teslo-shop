016ef7062d4b2ab4d74c9e7024decd5310fdd1f1
Gersomsim <gersomhernandez@me.com>
2023-07-15 21:58:32 -0600

    :sparkles: Cambiando el nombre

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -14,6 +14,7 @@ import { FilesService } from './files.service';
 import { FileInterceptor } from "@nestjs/platform-express";
 import { FileFilter } from "./helpers/fileFilter.helper";
 import { diskStorage } from "multer";
+import { FileNamer } from "./helpers/fileNamer.helper";
 
 @Controller('files')
 export class FilesController {
@@ -28,6 +29,7 @@ export class FilesController {
       },
       storage: diskStorage({
         destination: './static/products',
+        filename: FileNamer,
       }),
     }),
   )
@@ -38,6 +40,7 @@ export class FilesController {
     if (!file) {
       throw new BadRequestException('Make sure that the file is an image');
     }
+    console.log(file);
     return {
       fileName: file.originalname,
     };
---
+++ src/files/helpers/fileNamer.helper.ts
@@ -0,0 +1,13 @@
+
+import { v4 as uuid } from 'uuid'
+export const FileNamer = (
+  req: Express.Request,
+  file: Express.Multer.File,
+  callback: Function,
+) => {
+  if (!file) return callback(new Error('File is empty'), false);
+  const extension = file.mimetype.split('/')[1];
+  const filename = `${uuid()}.${extension}`;
+
+  callback(null, filename);
+};
