b583c47c97a0fb4040aae1ce684b4ddde6981eaf
Gersomsim <gersomhernandez@me.com>
2023-07-20 12:53:40 -0600

    :sparkles: Un decorador para tres :)

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,14 +1,15 @@
-import { Body, Controller, Get, Post, UseGuards } from "@nestjs/common";
-import { AuthService } from "./auth.service";
-import { CreateUserDto } from "./dto/create-user.dto";
-import { LoginUserDto } from "./dto/login-user.dto";
-import { AuthGuard } from "@nestjs/passport";
-import { GetUser } from "./decorators/get-user.decorator";
-import { User } from "./entities/user.entity";
-import { RawHeaders } from "./decorators/raw-headers.decorator";
-import { UserRoleGuard } from "./guards/user-role.guard";
-import { RoleProtected } from "./decorators/role-protected.decorator";
-import { ValidRoles } from "./interfaces/valid-roles.interface";
+import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { LoginUserDto } from './dto/login-user.dto';
+import { AuthGuard } from '@nestjs/passport';
+import { GetUser } from './decorators/get-user.decorator';
+import { User } from './entities/user.entity';
+import { RawHeaders } from './decorators/raw-headers.decorator';
+import { UserRoleGuard } from './guards/user-role.guard';
+import { RoleProtected } from './decorators/role-protected.decorator';
+import { ValidRoles } from './interfaces/valid-roles.interface';
+import { Auth } from './decorators/auth.decorator';
 
 @Controller('auth')
 export class AuthController {
@@ -25,21 +26,24 @@ export class AuthController {
 
   @Get('test')
   @UseGuards(AuthGuard())
-  testing(
-    @GetUser() user: User,
-    @RawHeaders() raw: any
-  ) {
-
+  testing(@GetUser() user: User, @RawHeaders() raw: any) {
     return { user, raw };
   }
-  @Get('test2')
+
+
   // @SetMetadata('roles', ['user', 'admin'])
+  @Get('test2')
   @UseGuards(AuthGuard(), UserRoleGuard)
   @RoleProtected(ValidRoles.admin)
-  testingdos(
-    @GetUser() user: User,
-  ) {
-
+  testingdos(@GetUser() user: User) {
+    return {
+      ok: true,
+      user,
+    };
+  }
+  @Get('test3')
+  @Auth()
+  testingtres(@GetUser() user: User) {
     return {
       ok: true,
       user,
