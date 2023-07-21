da3310aab37e1f9c3ee2f82bea91cb6f92258e07
Gersomsim <gersomhernandez@me.com>
2023-07-20 19:07:29 -0600

    :closed_lock_with_key: Decoradores y Guards para proteccion de rutas

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,14 +1,17 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, ParseUUIDPipe, Query } from "@nestjs/common";
-import { ProductsService } from './products.service';
-import { CreateProductDto } from './dto/create-product.dto';
-import { UpdateProductDto } from './dto/update-product.dto';
+import { Body, Controller, Delete, Get, Param, ParseUUIDPipe, Patch, Post, Query } from "@nestjs/common";
+import { ProductsService } from "./products.service";
+import { CreateProductDto } from "./dto/create-product.dto";
+import { UpdateProductDto } from "./dto/update-product.dto";
 import { PaginationDto } from "../common/dto/pagination.dto";
+import { Auth } from "../auth/decorators/auth.decorator";
+import { ValidRoles } from "../auth/interfaces/valid-roles.interface";
 
 @Controller('products')
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
 
   @Post()
+  @Auth(ValidRoles.admin)
   create(@Body() createProductDto: CreateProductDto) {
     return this.productsService.create(createProductDto);
   }
@@ -33,6 +36,7 @@ export class ProductsController {
   }
 
   @Delete(':id')
+  @Auth(ValidRoles.admin)
   remove(@Param('id', ParseUUIDPipe) id: string) {
     return this.productsService.remove(id);
   }
--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -4,10 +4,12 @@ import { ProductsService } from './products.service';
 import { ProductsController } from './products.controller';
 import { Product } from "./entities/product.entity";
 import { ProductImage } from "./entities/product-image.entity";
+import { AuthModule } from "../auth/auth.module";
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([Product, ProductImage]),
+    AuthModule,
   ],
   controllers: [ProductsController],
   providers: [ProductsService],
